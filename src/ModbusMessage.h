4e1fe7e2ae506a17bcc287432ed9fd63872926f9
Miq1 <miq1@gmx.de>
2020-09-04 15:36:34 +0200

    Added token handling to factory methods

--- src/ModbusMessage.h
+++ src/ModbusMessage.h
@@ -80,20 +80,39 @@ protected:
 };
 
 class ModbusRequest : public ModbusMessage {
-public:
+protected:
   // getToken() - return this request's token value
   uint32_t getToken();
-protected:
+  // Factory methods to create valid Modbus messages from the parameters
+  // 1. no additional parameter (FCs 0x07, 0x0b, 0x0c, 0x11)
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint32_t tokenValue = 0);
+  
+  // 2. one uint16_t parameter (FC 0x18)
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint16_t p1, uint32_t tokenValue = 0);
+  
+  // 3. two uint16_t parameters (FC 0x01, 0x02, 0x03, 0x04, 0x05, 0x06)
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint16_t p1, uint16_t p2, uint32_t tokenValue = 0);
+  
+  // 4. three uint16_t parameters (FC 0x16)
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint16_t p1, uint16_t p2, uint16_t p3, uint32_t tokenValue = 0);
+  
+  // 5. two uint16_t parameters, a uint8_t length byte and a uint8_t* pointer to array of bytes (FC 0x0f)
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint16_t p1, uint16_t p2, uint8_t count, uint16_t *arrayOfWords, uint32_t tokenValue = 0);
+  
+  // 6. two uint16_t parameters, a uint8_t length byte and a uint16_t* pointer to array of values (FC 0x10)
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint16_t p1, uint16_t p2, uint8_t count, uint8_t *arrayOfBytes, uint32_t tokenValue = 0);
+
+  // 7. generic constructor for preformatted data ==> count is counting bytes!
+  static ModbusMessage *createMessage(Error& returnCode, uint8_t serverID, uint8_t functionCode, uint16_t count, uint8_t *arrayOfBytes, uint32_t tokenValue = 0);
   uint32_t MRQ_token;            // User defined token to uniquely identify request
   uint32_t MRQ_timeout;          // timeout value waiting for response
 
 };
 
 class ModbusResponse : public ModbusMessage {
-public:
+protected:
   // getError() - returns error code
   uint8_t getError();
-protected:
   ModbusRequest *MRS_request;    // Pointer to the request for this response
   uint8_t MRS_error;             // Error code (0 if ok)
 };
