e7c3d1d40720df9db0a03253e11b0faedc8ebca0
Miq1 <miq1@gmx.de>
2020-09-23 13:19:34 +0200

    Add example for TCP/WiFi/FC=3

---
+++ examples/TCP03example/main.cpp
@@ -0,0 +1,109 @@
+// =================================================================================================
+// ModbusClient: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to ModbusClient
+//               MIT license - see license.md for details
+// =================================================================================================
+
+// Example code to show the usage of the ModbusClient library. 
+// Please refer to root/Readme.md for a full description.
+
+// Includes: <Arduino.h> for Serial etc., WiFi.h for WiFi support
+#include <Arduino.h>
+#include <WiFi.h>
+
+// Include the header for the ModbusClient TCP style
+#include "ModbusTCP.h"
+
+WiFiClient theClient;                          // Set up a client
+char ssid[] = "...........";                   // SSID and ...
+char pass[] = "...........";                   // password for the WiFi network used
+
+// Create a ModbusTCP client instance
+ModbusTCP MB(theClient);
+
+// Define an onData handler function to receive the regular responses
+// Arguments are Modbus server ID, the function code requested, the message data and length of it, 
+// plus a user-supplied token to identify the causing request
+void handleData(uint8_t serverAddress, uint8_t fc, const uint8_t* data, uint16_t length, uint32_t token) 
+{
+  Serial.printf("Response: serverID=%d, FC=%d, Token=%08X, length=%d:\n", (unsigned int)serverAddress, (unsigned int)fc, token, length);
+  const uint8_t *cp = data;
+  uint16_t      i = length;
+  while (i--) {
+    Serial.printf("%02X ", *cp++);
+  }
+  Serial.println("");
+}
+
+// Define an onError handler function to receive error responses
+// Arguments are the error code returned and a user-supplied token to identify the causing request
+void handleError(Error error, uint32_t token) 
+{
+  // ModbusError wraps the error code and provides a readable error message for it
+  ModbusError me(error);
+  Serial.printf("Error response: %02X - %s\n", (int)me, (const char *)me);
+}
+
+// Setup() - initialization happens here
+void setup() {
+// Init Serial monitor
+  Serial.begin(115200);
+  while (!Serial) {}
+  Serial.println("__ OK __");
+
+// Connect to WiFi
+  WiFi.begin(ssid, pass);
+  delay(200);
+  int status = WiFi.status();
+  while (status != WL_CONNECTED) {
+    Serial.print(". ");
+    WiFi.disconnect(true);
+    WiFi.begin(ssid, pass);
+    delay(1000);
+    status = WiFi.status();
+  }
+  IPAddress wIP = WiFi.localIP();
+  Serial.printf("WIFi IP address: %u.%u.%u.%u\n", wIP[0], wIP[1], wIP[2], wIP[3]);
+
+// Set up ModbusTCP client.
+// - provide onData handler function
+  MB.onDataHandler(&handleData);
+// - provide onError handler function
+  MB.onErrorHandler(&handleError);
+// Set message timeout to 2000ms and interval between requests to the same host to 200ms
+  MB.setTimeout(2000, 200);
+// Start ModbusTCP background task
+  MB.begin();
+
+// Issue a request
+// Set Modbus TCP server address and port number
+// (Fill in your data here!)
+  MB.setTarget(IPAddress(0, 0, 0, 0), 0);
+
+// Create request for
+// (Fill in your data here!)
+// - server ID = 20
+// - function code = 0x03 (read holding register)
+// - start address to read = word 10
+// - number of words to read = 4
+// - token to match the response with the request. We take the current millis() value for it.
+//
+// If something is missing or wrong with the call parameters, we will immediately get an error code 
+// and the request will not be issued
+  Error err = MB.addRequest(20, READ_HOLD_REGISTER, 10, 4, (uint32_t)millis());
+  if (err!=SUCCESS) {
+    ModbusError e(err);
+    Serial.printf("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+  }
+
+// Else the request is processed in the background task and the onData/onError handler functions will get the result.
+//
+// The output on the Serial Monitor will be (depending on your WiFi and Modbus the data will be different):
+//     __ OK __
+//     . WIFi IP address: 192.168.178.74
+//     Response: serverID=20, FC=3, Token=0000056C, length=11:
+//     14 03 04 01 F6 FF FF FF 00 C0 A8
+}
+
+// loop() - nothing done here today!
+void loop() {
+}
