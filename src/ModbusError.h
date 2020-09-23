9b7aa2389b889d799f42e6e80ed87b22df751e44
Miq1 <miq1@gmx.de>
2020-09-23 11:14:45 +0200

    A missed copyright statement.

--- src/ModbusError.h
+++ src/ModbusError.h
@@ -1,3 +1,7 @@
+// =================================================================================================
+// ModbusClient: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to ModbusClient
+//               MIT license - see license.md for details
+// =================================================================================================
 #ifndef _MODBUS_ERROR_H
 #define _MODBUS_ERROR_H
 #include "ModbusTypeDefs.h"
