7f6e16eab40c566b7ec91838457522e0fcf9ce86
Miq1 <miq1@gmx.de>
2020-11-11 16:11:17 +0100

    Add runtime log level, hex dump and raw output to logging

--- src/Logging.h
+++ src/Logging.h
@@ -41,48 +41,74 @@ constexpr const char* file_name(const char* str) {
 #define LOCAL_LOG_LEVEL LOG_LEVEL
 #endif
 
-#define LOGLINE_T(x, format, ...) LOGDEVICE.printf("[" #x "] %s:%d:%s " format, file_name(__FILE__), __LINE__, __func__, ##__VA_ARGS__)
+extern int MBUlogLvl;
+void logHexDump(Print& output, const char *letter, const char *label, const uint8_t *data, const size_t length);
+
+#define LOG_LINE_T(level, x, format, ...) if (MBUlogLvl >= level) LOGDEVICE.printf("[" #x "] %s:%d:%s " format, file_name(__FILE__), __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_RAW_T(level, x, format, ...) if (MBUlogLvl >= level) LOGDEVICE.printf(format, ##__VA_ARGS__)
+#define HEX_DUMP_T(x, level, label, address, length) if (MBUlogLvl >= level) logHexDump(LOGDEVICE, #x, #label, address, length)
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_NONE
-#define LOG_N(format, ...) LOGLINE_T(N, format, ##__VA_ARGS__)
+#define LOG_N(format, ...) LOG_LINE_T(LOG_LEVEL_NONE, N, format, ##__VA_ARGS__)
+#define LOGRAW_N(format, ...) LOG_RAW_T(LOG_LEVEL_NONE, N, format, ##__VA_ARGS__)
+#define HEXDUMP_N(label, address, length) HEX_DUMP_T(N, LOG_LEVEL_NONE, label, address, length)
 #else
 #define LOG_N(format, ...)
+#define LOGRAW_N(format, ...)
 #endif
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_CRITICAL
-#define LOG_C(format, ...) LOGLINE_T(C, format, ##__VA_ARGS__)
+#define LOG_C(format, ...) LOG_LINE_T(LOG_LEVEL_CRITICAL, C, format, ##__VA_ARGS__)
+#define LOGRAW_C(format, ...) LOG_RAW_T(LOG_LEVEL_CRITICAL, C, format, ##__VA_ARGS__)
+#define HEXDUMP_C(label, address, length) HEX_DUMP_T(C, LOG_LEVEL_CRITICAL, label, address, length)
 #else
 #define LOG_c(format, ...)
+#define LOGRAW_C(format, ...)
 #endif
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_ERROR
-#define LOG_E(format, ...) LOGLINE_T(E, format, ##__VA_ARGS__)
+#define LOG_E(format, ...) LOG_LINE_T(LOG_LEVEL_ERROR, E, format, ##__VA_ARGS__)
+#define LOGRAW_E(format, ...) LOG_RAW_T(LOG_LEVEL_ERROR, E, format, ##__VA_ARGS__)
+#define HEXDUMP_E(label, address, length) HEX_DUMP_T(E, LOG_LEVEL_ERROR, label, address, length)
 #else
 #define LOG_E(format, ...)
+#define LOGRAW_E(format, ...)
 #endif
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_WARNING
-#define LOG_W(format, ...) LOGLINE_T(W, format, ##__VA_ARGS__)
+#define LOG_W(format, ...) LOG_LINE_T(LOG_LEVEL_WARNING, W, format, ##__VA_ARGS__)
+#define LOGRAW_W(format, ...) LOG_RAW_T(LOG_LEVEL_WARNING, W, format, ##__VA_ARGS__)
+#define HEXDUMP_W(label, address, length) HEX_DUMP_T(W, LOG_LEVEL_WARNING, label, address, length)
 #else
 #define LOG_W(format, ...)
+#define LOGRAW_W(format, ...)
 #endif
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_INFO
-#define LOG_I(format, ...) LOGLINE_T(I, format, ##__VA_ARGS__)
+#define LOG_I(format, ...) LOG_LINE_T(LOG_LEVEL_INFO, I, format, ##__VA_ARGS__)
+#define LOGRAW_I(format, ...) LOG_RAW_T(LOG_LEVEL_INFO, I, format, ##__VA_ARGS__)
+#define HEXDUMP_I(label, address, length) HEX_DUMP_T(I, LOG_LEVEL_INFO, label, address, length)
 #else
 #define LOG_I(format, ...)
+#define LOGRAW_I(format, ...)
 #endif
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_DEBUG
-#define LOG_D(format, ...) LOGLINE_T(D, format, ##__VA_ARGS__)
+#define LOG_D(format, ...) LOG_LINE_T(LOG_LEVEL_DEBUG, D, format, ##__VA_ARGS__)
+#define LOGRAW_D(format, ...) LOG_RAW_T(LOG_LEVEL_DEBUG, D, format, ##__VA_ARGS__)
+#define HEXDUMP_D(label, address, length) HEX_DUMP_T(D, LOG_LEVEL_DEBUG, label, address, length)
 #else
 #define LOG_D(format, ...)
+#define LOGRAW_D(format, ...)
 #endif
 
 #if LOCAL_LOG_LEVEL >= LOG_LEVEL_VERBOSE
-#define LOG_V(format, ...) LOGLINE_T(V, format, ##__VA_ARGS__)
+#define LOG_V(format, ...) LOG_LINE_T(LOG_LEVEL_VERBOSE, V, format, ##__VA_ARGS__)
+#define LOGRAW_V(format, ...) LOG_RAW_T(LOG_LEVEL_VERBOSE, V, format, ##__VA_ARGS__)
+#define HEXDUMP_V(label, address, length) HEX_DUMP_T(v, LOG_LEVEL_VERBOSE, label, address, length)
 #else
 #define LOG_V(format, ...)
+#define LOGRAW_V(format, ...)
 #endif
 
 #endif
