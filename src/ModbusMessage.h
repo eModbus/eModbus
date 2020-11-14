fd1c17463b1866e45c106cec5bce461bf3436403
Miq1 <miq1@gmx.de>
2020-11-14 16:51:37 +0100

    Remove dump() - obsolete now with HEXDUMP_N()

--- src/ModbusMessage.h
+++ src/ModbusMessage.h
@@ -80,10 +80,6 @@ protected:
   
   virtual void isInstance() = 0;   // Make this class abstract
 
-#ifdef TESTING
-  void dump(const char *header);
-#endif
-  
   uint8_t   *MM_data;            // Message data buffer
   uint16_t    MM_len;            // Allocated length of MM_data
   uint16_t  MM_index;            // Pointer into MM_data
