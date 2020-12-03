850edf52b06ced7b0d6c4c0223c193c9b759838e
Miq1 <miq1@gmx.de>
2020-12-03 16:59:16 +0100

    Adapt to new ModbusMessage

--- examples/RTU06example/main.cpp
+++ examples/RTU06example/main.cpp
@@ -19,13 +19,11 @@ ModbusClientRTU MB(Serial2);
 // Define an onData handler function to receive the regular responses
 // Arguments are Modbus server ID, the function code requested, the message data and length of it, 
 // plus a user-supplied token to identify the causing request
-void handleData(uint8_t serverAddress, uint8_t fc, const uint8_t* data, uint16_t length, uint32_t token) 
+void handleData(ModbusMessage response, uint32_t token) 
 {
-  Serial.printf("Response: serverID=%d, FC=%d, Token=%08X, length=%d:\n", (unsigned int)serverAddress, (unsigned int)fc, token, length);
-  const uint8_t *cp = data;
-  uint16_t      i = length;
-  while (i--) {
-    Serial.printf("%02X ", *cp++);
+  Serial.printf("Response: serverID=%d, FC=%d, Token=%08X, length=%d:\n", response.getServerID(), response.getFunctionCode(), token, response.size());
+  for (auto& byte : response) {
+    Serial.printf("%02X ", byte);
   }
   Serial.println("");
 }
@@ -60,57 +58,56 @@ void setup() {
 // Start ModbusRTU background task
   MB.begin();
 
-// We will first read the register, then write to it and finally read it again to verify the change
+// We will first set the register to a known state, read the register, 
+// then write to it and finally read it again to verify the change
 
-// Create request for
+// Set defined conditions first - write 0x1234 to the register
+  uint32_t Token = 1111;
+
+  Error err = MB.addRequest(Token++, 1, WRITE_HOLD_REGISTER, 10, 0x1234);
+  if (err!=SUCCESS) {
+    ModbusError e(err);
+    Serial.printf("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+  }
+
+// Create request by
 // (Fill in your data here!)
+// - token to match the response with the request.
 // - server ID = 1
 // - function code = 0x03 (read holding register)
 // - address to read = word 10
 // - data words to read = 1
-// - token to match the response with the request.
 //
 // If something is missing or wrong with the call parameters, we will immediately get an error code 
 // and the request will not be issued
-  uint32_t Token = 1111;
 
-  Error err = MB.addRequest(1, READ_HOLD_REGISTER, 10, 1, Token++);
+// Read register
+  err = MB.addRequest(Token++, 1, READ_HOLD_REGISTER, 10, 1);
   if (err!=SUCCESS) {
     ModbusError e(err);
     Serial.printf("Error creating request: %02X - %s\n", (int)e, (const char *)e);
   }
 
-// Create request for
-// (Fill in your data here!)
-// - server ID = 1
-// - function code = 0x06 (write holding register)
-// - address to write = word 10
-// - data word to write = 0xBEEF
-// - token to match the response with the request.
-//
-  err = MB.addRequest(1, WRITE_HOLD_REGISTER, 10, 0xBEEF, Token++);
+// Now write 0xBEEF to it
+  err = MB.addRequest(Token++, 1, WRITE_HOLD_REGISTER, 10, 0xBEEF);
   if (err!=SUCCESS) {
     ModbusError e(err);
     Serial.printf("Error creating request: %02X - %s\n", (int)e, (const char *)e);
   }
 
-// Create request for
-// (Fill in your data here!)
-// - server ID = 1
-// - function code = 0x03 (read holding register)
-// - address to read = word 10
-// - data words to read = 1
-// - token to match the response with the request.
-//
-  err = MB.addRequest(1, READ_HOLD_REGISTER, 10, 1, Token++);
+// Read it again to verify
+  err = MB.addRequest(Token++, 1, READ_HOLD_REGISTER, 10, 1);
   if (err!=SUCCESS) {
     ModbusError e(err);
     Serial.printf("Error creating request: %02X - %s\n", (int)e, (const char *)e);
   }
 
 // The output on the Serial Monitor will be (depending on your Modbus the data will be different):
+//      __ OK __
+//      Response: serverID=1, FC=6, Token=00000457, length=6:
+//      01 06 00 0A 12 34
 //      Response: serverID=1, FC=3, Token=00000458, length=5:
-//      01 03 02 32 33
+//      01 03 02 12 34
 //      Response: serverID=1, FC=6, Token=00000459, length=6:
 //      01 06 00 0A BE EF
 //      Response: serverID=1, FC=3, Token=0000045A, length=5:
