ad6bfeaa9af47fac462b4772c91964d01c3a8ec8
Miq1 <miq1@gmx.de>
2021-05-14 16:39:05 +0200

    Fix crash with TCP bridge

--- src/ModbusBridgeTemp.h
+++ src/ModbusBridgeTemp.h
@@ -174,7 +174,7 @@ ModbusMessage ModbusBridge<SERVERCLASS>::bridgeWorker(ModbusMessage msg) {
     LOG_D("Request (%02X/%02X) sent\n", servers[aliasID]->serverID, functionCode);
     // TCP servers have a target host/port that needs to be set in the client
     if (servers[aliasID]->serverType == TCP_SERVER) {
-      reinterpret_cast<ModbusClientTCP *>(servers[aliasID]->client)->syncRequestMT(msg, (uint32_t)millis(), servers[aliasID]->host, servers[aliasID]->port);
+      response = reinterpret_cast<ModbusClientTCP *>(servers[aliasID]->client)->syncRequestMT(msg, (uint32_t)millis(), servers[aliasID]->host, servers[aliasID]->port);
     } else {
       response = servers[aliasID]->client->syncRequestM(msg, (uint32_t)millis());
     }
